6cac15a00011b54c97eb656dc27a82f88a67e995
machan201708 <1084109336@qq.com>
2019-03-05 19:47:55 +0800

    tabs

--- src/components/containers/home/home.js
+++ src/components/containers/home/home.js
@@ -5,6 +5,8 @@ import ImageSlider from './image-slider';
 import ListImgBtn from './list-img-btn';
 
 import '../../../static/styles/app.scss';
+import ListImg from "./list-img";
+import {Tabs} from 'antd';
 
 class Home extends React.Component {
   constructor() {
@@ -16,23 +18,41 @@ class Home extends React.Component {
         {id: 2, src: '/images/banner-3.jpg', link: ''},
         {id: 3, src: '/images/banner-4.jpg', link: ''}
       ],
+      types: [0, 1, 2, 3, 4],
       homeList: [
-        {id: 0, src: '', title: '', link: '', description: ''},
-        {id: 1, src: '', title: '', link: '', description: ''},
-        {id: 2, src: '', title: '', link: '', description: ''},
-        {id: 3, src: '', title: '', link: '', description: ''},
-        {id: 4, src: '', title: '', link: '', description: ''},
-        {id: 5, src: '', title: '', link: '', description: ''},
-      ]
+        {id: 0, src: './images/film-1.jpg', title: '', link: '', description: ''},
+        {id: 1, src: './images/film-2.jpg', title: '', link: '', description: ''},
+        {id: 2, src: './images/film-1.jpg', title: '', link: '', description: ''},
+        {id: 3, src: './images/film-2.jpg', title: '', link: '', description: ''},
+        {id: 4, src: './images/film-1.jpg', title: '', link: '', description: ''},
+        {id: 5, src: './images/film-2.jpg', title: '', link: '', description: ''},
+      ],
+      imgList: {
+        title: "",
+        items: [
+          {id: 0, src: './images/film-1.jpg', title: '', link: '', description: ''},
+          {id: 1, src: './images/film-2.jpg', title: '', link: '', description: ''},
+          {id: 2, src: './images/film-1.jpg', title: '', link: '', description: ''},
+          {id: 3, src: './images/film-2.jpg', title: '', link: '', description: ''},
+          {id: 4, src: './images/film-1.jpg', title: '', link: '', description: ''},
+          {id: 5, src: './images/film-2.jpg', title: '', link: '', description: ''}
+        ]
+      }
     }
   }
 
   render() {
+    const TabPane = Tabs.TabPane;
     return (
       <div className='container-wrapper'>
         <HeaderTransparent></HeaderTransparent>
         <ImageSlider imgList={this.state.imgList}></ImageSlider>
         <ListImgBtn homeList={this.state.homeList}></ListImgBtn>
+        <ListImg type={this.state.type[0]} list={}></ListImg>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="精选推荐" key="0">11</TabPane>
+          <TabPane tab="我关注的" key="1">22</TabPane>
+        </Tabs>
       </div>);
   }
 }
---
+++ src/components/containers/home/list-img.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ListImg extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const type = this.props.type;
+    const list = this.props.list;
+    return (
+      <div className="img-list-wrapper">
+
+      </div>
+    );
+  }
+}
+
+export default ListImg;
